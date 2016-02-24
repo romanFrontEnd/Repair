8755ca02078f9e01a17e5a19d86f27d2e7138a5e
romangl <rgluschuk@gmail.com>
2016-02-24 10:21:52 +0200

    add node mailer support and express

--- server/server.js
+++ server/server.js
@@ -1,11 +1,45 @@
-var express = require('express'),
-    app = express();
+var express = require('express');
+var nodemailer = require("nodemailer");
+var app = express();
+/*
+ Here we are configuring our SMTP Server details.
+ STMP is mail server which is responsible for sending and recieving email.
+ */
+var smtpTransport = nodemailer.createTransport("SMTP", {
+    service: "Gmail",
+    auth: {
+        user: "",
+        pass: ""
+    }
+});
+/*------------------SMTP Over-----------------------------*/
 
-app.use(express.static(__dirname + '/public'));
-
+/*------------------Routing Started ------------------------*/
 
 app.get('/', function (req, res) {
-  res.send('Hello, World!')
+    //res.sendfile('index.html');
+});
+app.get('/send', function (req, res) {
+    var mailOptions = {
+        to: req.query.to,
+        subject: req.query.subject,
+        text: req.query.text
+    };
+
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function (error, response) {
+        if (error) {
+            console.log(error);
+            res.end("error");
+        } else {
+            console.log("Message sent: " + response.message);
+            res.end("sent");
+        }
+    });
 });
 
-app.listen(8085);
+/*--------------------Routing Over----------------------------*/
+
+app.listen(8085, function () {
+    console.log("Express Started on Port 8085");
+});
